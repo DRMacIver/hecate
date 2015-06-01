cb121a3eb799491648273f01e009e9c53415feeb
David R. MacIver <david@drmaciver.com>
2015-06-01 15:22:54 +0100

    Fix indentation

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv local 3.4.3
 PYTHONPATH=src python -c '
-    from hecate.tmux import TMUX
-    print("tmux=", TMUX)
+from hecate.tmux import TMUX
+print("tmux=", TMUX)
 '
 tox
